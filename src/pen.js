f2e2297399b7d7d92e33c5fa4fb4148dbd53c3aa
Zack <yu521088@gmail.com>
2014-04-17 17:25:05 +0800

    update html

--- src/pen.js
+++ src/pen.js
@@ -309,7 +309,7 @@
       if(cmd === 'insertimage' && !!+[1,]){
         //IE don't support insertHTML
         cmd = 'insertHTML';
-        val = '<span class="img-content" alt="' + val + '" title="' + that._sel.toString() + '"></span><img src="' + val + '" alt="' + that._sel.toString() + '" title="' + that._sel.toString() + '" />';
+        val = '<span class="img-content" alt="' + val + '" title="' + that._sel.toString() + '"><img src="' + val + '" alt="' + that._sel.toString() + '" title="' + that._sel.toString() + '" /></span>';
       }
       if(document.execCommand(cmd, false, val) && that.config.debug) {
         utils.log('success' + message);
